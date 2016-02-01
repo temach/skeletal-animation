a2638245f4e4174002f6bac68b19b1fab81c6757
Artem Abramov <tematibr@gmail.com>
2016-02-01 07:13:15 +0300

    Added DrawToOpenGL class which will draw everything

---
+++ WinFormAnimation2D/DrawToOpenGL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormAnimation2D
+{
+    /// <summary>
+    /// Class to control openGL settings and do the actual drawing. 
+    /// All openGL calls will be here.
+    /// </summary>
+    class DrawToOpenGL
+    {
+    }
+}
