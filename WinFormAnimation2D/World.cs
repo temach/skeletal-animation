0c118de3899daeb63d7d35127149645d2238f9e0
Artem Abramov <tematibr@gmail.com>
2016-02-06 07:15:26 +0300

    Expand project description in World.cs

--- WinFormAnimation2D/World.cs
+++ WinFormAnimation2D/World.cs
@@ -14,24 +14,28 @@
 /// Assimp is row major. 
 /// OpenGL also uses row-major order.
 /// I do not know about OpenTK.
-/// Row-major or column-major determines how the matrices are stored in memory. 
+/// Row-major or column-major is about how the matrices are stored in memory. 
 /// This is important when we want to load them into OpenGL which expects them row-major.
 ///
 /// But we also have some differences as to how where the translation of matrix is stored.
 /// Is the translation in right most column? Or is it in the bottom-row?
-/// OpenTK uses the bottom row to store translation.
+/// OpenTK uses the column major format (bottom row stores translation):
+///  X1  X2  X3   0
+///  Y1  Y2  Y3   0
+///  Z1  Z2  Z3   0
+///  T1  T2  T3   1
+/// So OpenTK matrix in memory: [X1 X2 X3 0][Y1 Y2 Y3 0][Z1 Z2 Z3 0][T1 T2 T3 1]
+/// Where vector X = (x1, x2, x3)
+/// Vector Y = (y1, y2, y3)
+/// Vector Z = (y1, y2, y3)
+/// translation T = (T1, T2, T3)
 /// Assimp uses the righ most column to store translation.
 /// This means that matrices look like this in Assimp:
 ///  X1  Y1  Z1  T1
 ///  X2  Y2  Z2  T2
 ///  X3  Y3  Z3  T3
 ///  0   0   0   1
-/// Where vector X = (x1, x2, x3)
-/// Vector Y = (y1, y2, y3)
-/// Vector Z = (y1, y2, y3)
-/// translation T = (T1, T2, T3)
-///
-/// So in memory this looks like: [X1 Y1 Z1 T1] [X2 Y2 Z2 T2] [X3 Y3 Z3 T3] [0  0  0  1]
+/// So in memory Assimp matrix looks like: [X1 Y1 Z1 T1] [X2 Y2 Z2 T2] [X3 Y3 Z3 T3] [0  0  0  1]
 /// Which is called row-major. Get it now? ;)
 
 /// Lol, I found out why open3mod always had to use a Transpose on OpenTK matrix in the rendering loop. 
@@ -41,6 +45,7 @@
 /// For singletons instead of constructor throwing an exception we will use readonly fields. 
 /// Maybe this will be cleaner.
 
+/// TODO: Get rid of assimp matrices and vectors.
 
 
 
